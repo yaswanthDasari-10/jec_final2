ef255060083dcb3da0a7de628817a8abdcb8beeb
yaswanthDasari-10 <dasariswanth@gmail.com1>
2026-04-24 21:30:09 +0530

    replaced frontend with production url

--- Eligitrack-main/jec_frontend/front/src/services/api.js
+++ Eligitrack-main/jec_frontend/front/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://jec-final2.onrender.com/api" });
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
