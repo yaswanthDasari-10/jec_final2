b68e1eebdd1141717288285b13204fc04fe61df4
yaswanthDasari-10 <dasariswanth@gmail.com1>
2026-04-25 09:26:47 +0530

    replaced frontend with production url

--- Eligitrack-main/backend/server.js
+++ Eligitrack-main/backend/server.js
@@ -8,10 +8,10 @@ const app = express();
 
 // ── CORS (allow frontend on any port) ──
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:5174","https://jec-final2.onrender.com"],
   credentials: true,
 }));
-
+app.options("*",cors())
 // ── Body parser ──
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
